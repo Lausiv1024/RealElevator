e6044977ed42b721a8f6834112b3d042981e661b
lausiv1024 <75715137+Lausiv1024@users.noreply.github.com>
2021-09-14 19:51:00 +0900

    Real Elevator

---
+++ src/main/java/lausiv1024/util/EleSurfaceType.java
@@ -0,0 +1,31 @@
+package lausiv1024.util;
+
+public enum EleSurface {
+    OAK,
+    DARK_OAK,
+    BIRCH,
+    JUNGLE,
+    ACACIA,
+    SPRUCE,
+    CRIMSON,
+    WARPED,
+    ANDESITE,
+    DIORITE,
+    GRANITE,
+    SMOOTH_STONE,
+    WHITE,
+    LIGHT_GRAY,
+    YELLOW,
+    PINK,
+    LIME,
+    CYAN,
+    BLUE,
+    BROWN,
+    RED,
+    GREEN,
+    GRAY,
+    BLACK,
+    STAINLESS
+
+    
+}
